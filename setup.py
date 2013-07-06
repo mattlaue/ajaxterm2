0014aafe2c5505760aaf7e3c518660b983b1f300
Matthew R. Laue <matt@akirisolutions.com>
2013-07-06 13:10:16 -0700

    Add the ini files and the www directory tree to data_files in the python setup script.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import os
 
 from setuptools import setup, find_packages
+from glob import glob
 
 setup(name='AjaxTerm2',
       version='1.0.0',
       py_modules=['ajaxterm2'],
       install_requires=['webob','paste','pastedeploy'],
+      data_files=[('', glob('*.ini')),
+                  ('www', glob('www/*.html')),
+                  ('www/js', glob('www/js/*.js')),
+                  ('www/css', glob('www/css/*.css')),
+                   ],
       )
