78459105c3e287562b0912234d8d33da1dc0dbce
Matthew R. Laue <matt@akirisolutions.com>
2013-07-06 12:13:52 -0700

    Add install requirements to the python setup script.

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ from setuptools import setup, find_packages
 setup(name='AjaxTerm2',
       version='1.0.0',
       py_modules=['ajaxterm2'],
+      install_requires=['webob','paste','pastedeploy'],
       )
