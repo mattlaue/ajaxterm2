441e16fa59ce0d543ec44a2ed994dd09476fe602
Matthew R. Laue <matt@akirisolutions.com>
2013-07-07 08:15:43 -0700

    Change version number to 1.1.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from glob import glob
 
 setup(name='AjaxTerm2',
-      version='1.0.0',
+      version='1.1.0',
       py_modules=['ajaxterm2'],
       install_requires=['webob','paste','pastedeploy'],
       data_files=[('', glob('*.ini')),
